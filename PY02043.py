f4e06d16777b8e93a5b4e2c2f42095d983ab2f9a
QuyetCN05 <buihuuquyet203@gmail.com>
2023-10-29 08:53:50 +0700

    add file

---
+++ PY02043.py
@@ -0,0 +1,10 @@
+from collections import *
+from functools import *
+from math import *
+import io, os, sys, time
+
+if __name__=='__main__':
+    for i in range(int(input())):
+        s=input()
+        n=input()
+        print(s.count(n))
