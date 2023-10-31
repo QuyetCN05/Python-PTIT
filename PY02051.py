b66059b4e34639ae85d2e569504d78f7f6ea4164
QuyetCN05 <buihuuquyet203@gmail.com>
2023-10-31 10:02:02 +0700

    Add file

---
+++ PY02051.py
@@ -0,0 +1,21 @@
+from collections import *
+from functools import *
+from math import *
+import io
+import os
+import sys
+import time
+
+if __name__ == '__main__':
+    n = int(sys.stdin.readline())
+    b = [[0 for _ in range(n)] for _ in range(n)]
+    for i in range(n): b[i] = list(map(int, sys.stdin.readline().split()))
+    a, s = [], 0
+    for i in range(n):
+        s += sum(b[i])
+        a.append(sum(b[i]))
+    if n == 2:
+        for x in a: print(x // 2, end = ' ')
+    else:
+        s = (s // 2 // (n - 1))
+        for x in a: print(((x - s) // (n - 2)), end = ' ')
